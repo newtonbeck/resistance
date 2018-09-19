a33b4eec1df9f98b3b259e04fa18b6292fef09e7
“Newton <newton.beck@gmail.com>
2018-09-19 05:25:01 -0300

    Shuffles resistance roles

--- index.js
+++ index.js
@@ -2,11 +2,50 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var game = {
-    players: {
-
+var rules = [
+    {
+        players: {
+            resistance: 3,
+            spies: 2,
+        },
+    },
+    {
+        players: {
+            resistance: 4,
+            spies: 2,
+        },
+    },
+    {
+        players: {
+            resistance: 4,
+            spies: 3,
+        },
+    },
+    {
+        players: {
+            resistance: 5,
+            spies: 3,
+        },
+    },
+    {
+        players: {
+            resistance: 6,
+            spies: 3,
+        },
+    },
+    {
+        players: {
+            resistance: 6,
+            spies: 4,
+        },
     }
+];
+
+var game = {
+    players: [ ]
 };
+
+var rule;
 
 app.get('/', function(_, res) {
     res.sendFile(__dirname + '/index.html');
@@ -14,7 +53,47 @@ app.get('/', function(_, res) {
 
 io.on('connection', function(socket) {
     socket.on('join game', function(player) {
-        game.players[socket.id] = player;
+        game.players.push({
+            socket: socket.id,
+            name: player
+        });
+    });
+
+    socket.on('start game', function() {
+        rule = rules[game.players.length - 5];
+
+        roles = Array(rule.players.resistance)
+            .fill('resistance')
+            .concat(
+                Array(rule.players.spies)
+                    .fill('spy')
+            );
+
+        shuffledRoles = roles
+            .map(function(role) {
+                return { sort: Math.random(), role: role };
+            })
+            .sort(function(a, b) {
+                return a.sort - b.sort;
+            })
+            .map(function(element) {
+                return element.role;
+            });
+
+        game
+            .players
+            .forEach(function(player, index) {
+                player['role'] = shuffledRoles[index];
+                if (socket.id === player.socket) {
+                    socket
+                        .emit('game started', player.role);
+                } else {
+                    socket
+                        .broadcast
+                        .to(player.socket)
+                        .emit('game started', player.role);
+                }
+            });
     });
 });
 
